daf8bce2c264a766e309d90208b02deb952993be
Muhammad Hasnain <hasnain.xwave@gmail.com>
2024-12-14 16:15:35 +0500

    Project Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Homepage from './components/homepage/homepage';
 
 function App() {
   return (
     <div className="App">
-      
+        <Homepage />
     </div>
   );
 }
