edce4f427f9df81649a11522ad78d7c1db5f9c5c
Muhammad Hasnain <hasnain.xwave@gmail.com>
2024-12-24 15:41:20 +0500

    work updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import HeaderSection from "./components/HeaderSection/HeaderSection";
 import { HeroSection } from "./components/HeroSection/HeroSection";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-import AccountPage from "./components/auth/Account";
+import AccountManagement from "./components/auth/My Account/Account-Management";
+import Profile from "./components/auth/My Account/Updated-Profile"; 
+
+
 
 function App() {
   return (
@@ -15,7 +18,8 @@ function App() {
         <Route path="/" element={<HeroSection />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/account" element={<AccountPage />} />
+        <Route path="/account" element={<AccountManagement />} />
+        <Route path="/account/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
